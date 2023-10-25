942819791867ee347a5850ca2a0975ff4b3fdd0b
Leonel Nievas Boso <leo_93_t1@hotmail.com>
2023-10-25 17:48:36 -0300

    Camel case para todos los campos

--- src/lab1proyectofinal/entidades/Bombero.java
+++ src/lab1proyectofinal/entidades/Bombero.java
@@ -16,29 +16,29 @@ public class Bombero {
     private String nombreApellido;
     private LocalDate fechaNacimiento;
     private long telefono;
-    private int codigo_brigada;
+    private int codigoBrigada;
     private boolean estado;
 
     public Bombero() {
     }
 
-    public Bombero(int dni, String nombreApellido, LocalDate fechaNacimiento, long telefono, int codigo_brigada, boolean estado) {
+    public Bombero(int dni, String nombreApellido, LocalDate fechaNacimiento, long telefono, int codigoBrigada, boolean estado) {
         this.idBombero = -1;
         this.dni = dni;
         this.nombreApellido = nombreApellido;
         this.fechaNacimiento = fechaNacimiento;
         this.telefono = telefono;
-        this.codigo_brigada = codigo_brigada;
+        this.codigoBrigada = codigoBrigada;
         this.estado = estado;
     }
 
-    public Bombero(int idBombero, int dni, String nombreApellido, LocalDate fechaNacimiento, long telefono, int codigo_brigada, boolean estado) {
+    public Bombero(int idBombero, int dni, String nombreApellido, LocalDate fechaNacimiento, long telefono, int codigoBrigada, boolean estado) {
         this.idBombero = idBombero;
         this.dni = dni;
         this.nombreApellido = nombreApellido;
         this.fechaNacimiento = fechaNacimiento;
         this.telefono = telefono;
-        this.codigo_brigada = codigo_brigada;
+        this.codigoBrigada = codigoBrigada;
         this.estado = estado;
     }
 
@@ -82,12 +82,12 @@ public class Bombero {
         this.telefono = telefono;
     }
 
-    public int getCodigo_brigada() {
-        return codigo_brigada;
+    public int getCodigoBrigada() {
+        return codigoBrigada;
     }
 
-    public void setCodigo_brigada(int codigo_brigada) {
-        this.codigo_brigada = codigo_brigada;
+    public void setCodigoBrigada(int codigoBrigada) {
+        this.codigoBrigada = codigoBrigada;
     }
 
     public boolean isEstado() {
@@ -100,7 +100,7 @@ public class Bombero {
 
     @Override
     public String toString() {
-        return "Bombero{" + "idBombero=" + idBombero + ", dni=" + dni + ", nombreApellido=" + nombreApellido + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", codigo_brigada=" + codigo_brigada + '}';
+        return "Bombero{" + "idBombero=" + idBombero + ", dni=" + dni + ", nombreApellido=" + nombreApellido + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", codigoBrigada=" + codigoBrigada + '}';
     }
 
 }
